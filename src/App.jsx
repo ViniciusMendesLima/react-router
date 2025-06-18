a141d920ba091e8410de4d7801852d31a579997d
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-06-18 16:48:30 -0300

    index pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Products from './pages/Products';
-import Error from './pages/Error';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Error from "./pages/Error";
+import SharedLayout from "./pages/SharedLayout";
 
 function App() {
   return (
     <BrowserRouter>
-    <nav>our navbar</nav>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='about' element={<About/>} />
-        <Route path='products' element={<Products/>} />
-        <Route path='*' element={<Error/>} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home/>}/>
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
-      <footer>our footer</footer>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
